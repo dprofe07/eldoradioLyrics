7eca2fd5d48300db9e82da96fca8cf05d371772f
dima <dprofe@yandex.ru>
2023-02-14 21:26:10 +0300

    add server prefix

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
+import os
+
 import flask
 from flask import render_template
 
 from eldoradioParser import eldoradio
 
-app = flask.Flask(__name__)
+if os.path.exists('/SERVER/is_server'):
+    prefix = '/eldoradio-lyrics'
+else:
+    prefix = ''
+
+app = flask.Flask(__name__, static_url_path=f'{prefix}/static')
 
 
-@app.route('/')
+@app.route(f'{prefix}/')
 def page_index():
     song = eldoradio.get_curr_song()
     return render_template('song_info.html', song=song)
